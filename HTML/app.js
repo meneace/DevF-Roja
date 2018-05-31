f9a7918842bc3ff5592820880fc600cc8eb3ee65
meneace <j.mendez@outlook.com>
2018-05-31 09:16:53 -0500

    primer dia

---
+++ HTML/app.js
@@ -0,0 +1,27 @@
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//for (let indice=0; indice < numeros.length; indice ++){
+//console.log(numeros[indice]);
+//}
+
+
+//for (let numero in numeros){
+//  console.log(numeros[numero]);
+//}
+//
+/*const valor = 2;
+numeros.forEach((numero) => {
+    for (let o = numero; o => 10; o++) {
+
+        for (let w = numero; w => 10; w++) {
+            console.log(w);
+        }
+    }
+})
+*/
+for (let i = 1; i <= 1; i++) {
+    for (let o = i; o <= 10; o++) {
+
+        console.log(o + " x", 7, "=", 7 * o);
+
+    }
+}
---
+++ HTML/cine.js
@@ -0,0 +1,10 @@
+let edad = -1;
+
+if (edad < 0) {
+    console.log("No se puede por numeros negativos");
+} else if (edad < 15) {
+    console.log("Eres un niño");
+} else if (edad < 18) {
+    console.log("Aun no eres mayor de edad")
+} else(
+    console.log("Puedes ver todas"))
