ed136cb9d995e90f7e7713219914cc7bfc9163df
meneace <j.mendez@outlook.com>
2018-05-31 09:57:48 -0500

    comentarios, input

--- HTML/multiplica.js
+++ HTML/multiplica.js
@@ -1,11 +1,14 @@
+//Seccion donde se crea variable a solicitar por consola
+
 const readline = require('readline');
 
+//Se habilita la captura y la salida de un valor
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-
+//Se hace uso del valor capturado
 rl.question('Dame un valor a multiplicar ', (valor) => {
 
     if (valor < 0) {
@@ -19,6 +22,6 @@ rl.question('Dame un valor a multiplicar ', (valor) => {
             }
         };
     }
-
+    //Se cierra conexion. Es muy importante cerrar las conexiones porque de lo contrario se quedaran abiertas epserando siempre un valor
     rl.close();
 })
