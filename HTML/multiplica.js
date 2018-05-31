0f5a17d2f5bc5be8aa616d25b2268d6831e8768c
meneace <j.mendez@outlook.com>
2018-05-31 09:54:28 -0500

    con input

--- HTML/multiplica.js
+++ HTML/multiplica.js
@@ -1,13 +1,24 @@
-let valor = 2;
+const readline = require('readline');
 
-if (valor < 0) {
-    console.log("No se puede multiplicar por numeros negativos");
-} else if (valor === 0) {
-    console.log("Todos los valores dan cero");
-} else {
-    for (let i = 1; i <= 1; i++) {
-        for (let o = i; o <= 10; o++) {
-            console.log(o + " x", valor, "=", valor * o);
-        }
-    };
-}
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
+rl.question('Dame un valor a multiplicar ', (valor) => {
+
+    if (valor < 0) {
+        console.log("No se puede multiplicar por numeros negativos");
+    } else if (valor === 0) {
+        console.log("Todos los valores dan cero");
+    } else {
+        for (let i = 1; i <= 1; i++) {
+            for (let o = i; o <= 10; o++) {
+                console.log(o + " x", valor, "=", valor * o);
+            }
+        };
+    }
+
+    rl.close();
+})
